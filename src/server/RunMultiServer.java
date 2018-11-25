0e146241311da5bf5b76416b75d745f1e920c865
Bar Zrihan <barzrcom@cs.colman.ac.il>
2018-11-25 20:57:16 +0200

    Add documentation to PipeMultiServer

--- src/server/RunMultiServer.java
+++ src/server/RunMultiServer.java
@@ -4,7 +4,10 @@ public class RunMultiServer {
     public static void main(String[] args) throws Exception {
         System.out.println("**** Server Side ****");
 
-        Server s = new PipeMultiServer(6400, 5);//Take the port from the args
+        /* Signature:
+        *  PipeMultiServer(portNumber, numberOfThreads)
+        */
+        Server s = new PipeMultiServer(6400, 5);
         s.start(new PipeClientHandler());
         System.in.read();
         s.stop();
